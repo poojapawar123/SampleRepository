10a129633e56bfba0070a26589a3895bce57eb39
poojapawar123 <31065860+poojapawar123@users.noreply.github.com>
2017-09-12 11:59:34 -0700

    first commit

---
+++ src/hello/Multiplication.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		int a=8,b=5,c=0;
+		System.out.println("Hello github");
+		c=a*b;
+		System.out.println("Multiplication is:"+c);
+		
+
+		
+	}
+
+}
